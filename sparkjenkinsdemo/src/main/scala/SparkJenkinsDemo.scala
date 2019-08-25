a27545948439ab464c1a31edfe71e3a6cbe17a70
Amir Ibrahim <amiribrahim@Amirs-MBP.fios-router.home>
2019-08-25 18:09:43 -0400

    l

--- sparkjenkinsdemo/src/main/scala/SparkJenkinsDemo.scala
+++ sparkjenkinsdemo/src/main/scala/SparkJenkinsDemo.scala
@@ -4,8 +4,9 @@ object SparkJenkinsDemo {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Spark_Jenkins_Demo").master("local[*]").getOrCreate()
-    spark.read.option("header","true").csv("/Users/amiribrahim/Desktop/jenkins_training/spark_github/Stocks/*")
-      .show()
+    val df = spark.read.option("header","true").csv("/Users/amiribrahim/Desktop/jenkins_training/spark_github/Stocks/*")
+
+    println(df.count())
   }
 
 }
