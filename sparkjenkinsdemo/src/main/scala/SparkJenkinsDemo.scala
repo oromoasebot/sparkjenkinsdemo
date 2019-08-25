2bd5ad7a5bd70e8609879589a319e161f1a5956a
oromoasebot <amirnamerica@gmail.com>
2019-08-25 19:17:06 -0400

    Update SparkJenkinsDemo.scala

--- sparkjenkinsdemo/src/main/scala/SparkJenkinsDemo.scala
+++ sparkjenkinsdemo/src/main/scala/SparkJenkinsDemo.scala
@@ -4,7 +4,7 @@ object SparkJenkinsDemo {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Spark_Jenkins_Demo").master("local[*]").getOrCreate()
-    val df = spark.read.option("header","true").csv("/Users/amiribrahim/Desktop/jenkins_training/spark_github/Stocks/*")
+    val df = spark.read.option("header","true").csv("/Users/${USER}/Desktop/jenkins_training/spark_github/Stocks/*")
 
     println("Printing total records...")
     println(df.count())
